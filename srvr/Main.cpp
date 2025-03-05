5c756db12329ac889dc246d9861799d75dc9473f
EdIsAvailable <nemoed@gmail.com>
2025-03-05 19:09:45 +0000

    First changes

--- srvr/Main.cpp
+++ srvr/Main.cpp
@@ -1,24 +1,19 @@
 #include <cstdio>
-#include<iostream>
+#include <iostream>
 #include "Acc.h"
 #include "Socket.h"
 #include "Chat.h"
 #include "Server.h"
 #include "db.h"
 #include "NewUser.h"
-
+#include "Logger.h"
 
 using namespace std;
 
 Server* _server;
-//Driver* driver;
+
 int main()
 {
-    //registerUser("Alice", "default_hash");
-    //registerUser("Bob", "default_hash");
-
-    //saveMessage("Alice", "Bob", "Hello, Bob!");
-    //fetchMessages("Bob");
 
     _server = new Server();
     _server->MainProcess();
