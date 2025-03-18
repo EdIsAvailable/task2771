db956904111725eed47ee83db7e80d148f14c12b
EdIsAvailable <nemoed@gmail.com>
2025-03-18 09:26:56 +0000

    Устранены мелкин баги

--- srvr/Logger.h
+++ srvr/Logger.h
@@ -1,26 +1,3 @@
-/*#pragma once
-
-#include <fstream>
-#include <string>
-#include <mutex>
-#include <shared_mutex>
-
-class Logger {
-public:
-    Logger(const std::string& filename = "log.txt");
-    ~Logger();
-
-    void writeLog(const std::string& message);
-    std::string readLog();
-    void resetReadPosition(); // метод для сброса указателя чтения
-    bool isEndOfFile();
-    
-private:
-    std::fstream logFile;
-    std::shared_mutex mutex_;
-};
-*/
-
 #pragma once
 
 #include <fstream>
--- srvr/db.h
+++ srvr/db.h
@@ -1,27 +1,5 @@
-/*#ifndef CHAT_DB_OPERATIONS_H
-#define CHAT_DB_OPERATIONS_H
+#pragma once
 
-#include <mysql_driver.h>
-#include <mysql_connection.h>
-#include <cppconn/prepared_statement.h>
-#include <cppconn/resultset.h>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <cppconn/driver.h>
-
-using namespace sql;
-using namespace std;
-unique_ptr<sql::Connection> connectToDatabase();
-void registerUser(const string& username, const string& passwordHash);
-void saveMessage(const string& sender, const string& receiver, const string& message);
-void fetchMessages(const string& receiver);
-
-#endif
-*/
-
-#ifndef CHAT_DB_OPERATIONS_H
-#define CHAT_DB_OPERATIONS_H
 
 #include <mysql/mysql.h>
 #include <mysql_driver.h>
@@ -38,5 +16,3 @@ std::unique_ptr<sql::Connection> connectToDatabase();
 void registerUser(const std::string& username, const std::string& passwordHash);
 void saveMessage(const std::string& sender, const std::string& receiver, const std::string& message);
 void fetchMessages(const std::string& receiver);
-
-#endif // CHAT_DB_OPERATIONS_H
